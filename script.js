92c2fe504153f7b6051808117eaed6f614ef469c
admareis <reis802@gmail.com>
2026-05-19 22:13:54 -0300

    layout disney

---
+++ script.js
@@ -0,0 +1,15 @@
+//const entradaUsuario = "mm";//
+//console.log(isNaN(entradaUsuario))//
+//const nome = "mickey";
+//const id = document.querySelector("#buscar");
+
+//console.log(id)
+
+//const urlString = `https://api.disneyapi.dev/character?name=${nome}`;
+//const urlId = `https://api.disneyapi.dev/characters/${id}`;
+
+//async function getUrl(url) {
+    //const dados = await((await fetch(url)).json())
+    //return dados;
+//}
+
